60fa1ad847ef0643f6812f5f7800376749b85eeb
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-03-03 14:39:37 -0800

    filled in fetch-utils.js for pokemon and star wars

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // import functions
 
 // grab DOM elements
+const template = document.querySelector('#template');
+const selector = document.querySelector('.api');
+const 
 
 // set event listeners 
     // get user input
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,7 +1,17 @@
-export async function getPokedex(){
+export async function getPokedex() {
     let url='https://pokedex-alchemy.herokuapp.com/api/pokedex';
 
-    const response = await fetch(url)
-    const jsonResponse = await response.json();
+    const resp = await fetch(url)
+    const json = await response.json();
     return json.results;
+}
+
+export async function getStarWarsPeople() {
+    let url = 'https://swapi.dev/api/planets';
+
+    const resp = await fetch(url);
+    const json = await resp.json();
+    return json.results;
+
+
 }
