3acc3c25f5484d713c1283ffd56c4535f4747820
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-03-03 16:17:13 -0800

    pokedex info rendering to webpage

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 // import functions
-
+import { getPokedex } from './fetch-utils.js';
 // grab DOM elements
 const template = document.querySelector('#template');
-const selector = document.querySelector('.api');
-const 
+const selector = document.querySelector('#api');
+const list = document.querySelector('#list');
+loadPokedex();
+
+async function loadPokedex() {
+    const pokedex = await getPokedex();
+    list.classList.add('pokemon');
+
+    for (let pokemon of pokedex) {
+        const clone = template.content.cloneNode(true);
+
+        const name = clone.querySelector('h2');
+        const image = clone.querySelector('img');
+        const type = clone.querySelector('h6');
+        
+        name.textContent = 'Name: ' + pokemon.pokemon;
+
+        type.textContent = 'Type: ' + pokemon.type;
+        image.src = pokemon.url_image;
+        image.alt = pokemon.pokedex;
+
+        list.appendChild(clone);
+    }
+}
+
 
 // set event listeners 
     // get user input
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,7 +1,7 @@
 export async function getPokedex() {
-    let url='https://pokedex-alchemy.herokuapp.com/api/pokedex';
+    let url = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
 
-    const resp = await fetch(url)
+    const response = await fetch(url);
     const json = await response.json();
     return json.results;
 }
